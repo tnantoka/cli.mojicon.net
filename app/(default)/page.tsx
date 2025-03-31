72585e5bef14a2383e31410de49c53d4d90b379b
tnantoka <tnantoka@bornneet.com>
2025-03-31 22:01:28 +0900

    feat: fix build error

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,12 +1,9 @@
 export const metadata = {
-  title: "Home - Simple",
-  description: "Page description",
+  title: "Mojicon CLI",
+  description: "Generate various icons with text and symbols from Google Fonts",
 };
 
 import Hero from "@/components/hero-home";
-import FeaturesPlanet from "@/components/features-planet";
-import LargeTestimonial from "@/components/large-testimonial";
-import Cta from "@/components/cta";
 
 export default function Home() {
   return (
